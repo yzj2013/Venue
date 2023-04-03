6fc4f3362cd53158504757d3f4b4d9a06ee03408
jennyyang <yzhenjuan@gmail>
2023-04-03 13:46:49 +1000

    Finished the carrousel

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import Carrousel from './Carrousel';
+
 const featured = () => {
-  return <div className='featured_container'></div>;
+  return (
+    <div className='featured_container'>
+      <Carrousel></Carrousel>
+      <div className='artist_name'>
+        <div className='wrapper'>Ariana Grande</div>
+      </div>
+    </div>
+  );
 };
+
+export default featured;
